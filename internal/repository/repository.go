b8aa85583ba5a68f2ebae2d5e3f15e2d5fd5cd54
Jaspier <dungoallen349@gmail.com>
2021-11-03 22:37:29 +0000

    Ability to search for availability by room and by all rooms

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "myapp/internal/models"
+import (
+	"myapp/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
