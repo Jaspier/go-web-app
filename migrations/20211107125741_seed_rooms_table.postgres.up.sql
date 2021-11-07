a6fe23578caba8ac6cf4b276809465d7dfc541bc
Jaspier <dungoallen349@gmail.com>
2021-11-07 13:03:18 +0000

    Adding migrations for seeding rooms and restrictions

---
+++ migrations/20211107125741_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at)
+VALUES ('Gamer''s Paradise', '2021-11-03 00:00:00.000', '2021-11-03 00:00:00.000'),
+       ('Gamer''s Suite', '2021-11-03 00:00:00.000', '2021-11-03 00:00:00.000');
