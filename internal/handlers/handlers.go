d9d0b98087b9122592e4ff889c45f71195d97915
Jaspier <dungoallen349@gmail.com>
2021-11-11 00:16:17 +0000

    Adding functionality to show 'new' reservations using a 'processed' column and listing them in admin panel

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -476,14 +476,12 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
+// AdminDashboard displays the admin dashboard
 func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-dashboard.page.gohtml", &models.TemplateData{})
 }
 
-func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "admin-new-reservations.page.gohtml", &models.TemplateData{})
-}
-
+// AdminAllReservations shows all reservations in admin tool
 func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
 	reservations, err := m.DB.AllReservations()
 	if err != nil {
@@ -495,6 +493,22 @@ func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request
 	data["reservations"] = reservations
 
 	render.Template(w, r, "admin-all-reservations.page.gohtml", &models.TemplateData{
+		Data: data,
+	})
+}
+
+// AdminNewReservations shows all new reservations
+func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
+	reservations, err := m.DB.AllNewReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["reservations"] = reservations
+
+	render.Template(w, r, "admin-new-reservations.page.gohtml", &models.TemplateData{
 		Data: data,
 	})
 }
